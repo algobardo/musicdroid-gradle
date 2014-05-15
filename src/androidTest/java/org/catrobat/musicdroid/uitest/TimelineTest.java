c75f04366b1437e38219a221fe2fbdf74ad53446
algobardo <mezzetti@gmail.com>
2014-05-15 14:28:25 +0200

    Test working, with errors

--- src/androidTest/java/org/catrobat/musicdroid/uitest/TimelineTest.java
+++ src/androidTest/java/org/catrobat/musicdroid/uitest/TimelineTest.java
@@ -20,7 +20,7 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
-package org.catrobat.musicdroid.uitest.menutest;
+package org.catrobat.musicdroid.uitest;
 
 import android.test.ActivityInstrumentationTestCase2;
 import android.util.Log;
